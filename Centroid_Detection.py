5375226cf584c6ba4385a3b28623fc79365af6ad
leebernard <leebernard@users.noreply.github.com>
2018-06-20 10:29:24 -0700

    Finish porting all code from the notebook. This does not contain all the data processing, but does include all code used.
    Future update should be to wrap this data processing routine as a function

--- Centroid_Detection.py
+++ Centroid_Detection.py
@@ -114,8 +114,80 @@ from astropy.visualization import SqrtStretch
 from astropy.visualization.mpl_normalize import ImageNormalize
 norm = ImageNormalize(stretch=SqrtStretch())
 
+# plt.figure()
+f1, axisarg = plt.subplots(2,1)
+
+axisarg[0].imshow(Object1_Data, norm=norm, origin='lower', cmap='viridis')
+axisarg[1].imshow(mask, origin='lower', cmap='viridis')
+# plt.show()
+
+
+
+
+# Centroid dectection by curve fitting
+# this attempt uses a bivariate normal distorbution as a model for the object
+
+# define gaussian function, assuming no correlation between x and y
+# indata is a pair of arrays, each array corresponding to the x indice or y indice, in the form (x, y)
+# amplitude is the maximum amplitude of the function, minus background
+# x0, y0 are the center coord. of the function
+# sigma_x, sigma_y are the widths of the function
+# offset is the background
+# the output is flattened, in order to package it for curve_fit
+def Gaussian_2d(indata, amplitude, x0, y0, sigma_x, sigma_y, offset):
+    import numpy as np
+    x, y = indata
+    normalize = 1 / (sigma_x * sigma_y * 2 * np.pi)
+
+    gaussian_fun = offset + amplitude * normalize * np.exp(
+        -(x - x0) ** 2 / (2 * sigma_x ** 2) - (y - y0) ** 2 / (2 * sigma_y ** 2))
+
+    return gaussian_fun.ravel()
+
+
+# fit data to gaussian
+from scipy.optimize import curve_fit
+from photutils import centroid_2dg
+
+# generate a best guess using photutils
+x_guess, y_guess = centroid_2dg(Object1_Data)
+amp_guess = np.amax(Object1_Data)
+
+# indexes of the apature, remembering that python indexes vert, horz
+y = np.arange(Object1_Data.shape[0])
+x = np.arange(Object1_Data.shape[1])
+x, y = np.meshgrid(x, y)
+
+# curve fit
+G_fit, G_cov = curve_fit(Gaussian_2d, (x, y), Object1_Data.ravel(), p0=[amp_guess, x_guess, y_guess, 1, 1, 1])
+print('Resultant parameters')
+print(G_fit)
+
+error = np.sqrt(np.diag(G_cov))
+print('Error on parameters')
+print(error)
+
+print('Covariance matrix, if that is interesting')
+print(G_cov)
+
+print('peak count')
+print(np.amax(Object1_Data))
+
+# display the result as a cross. The width of the lines correspond to the width in that direction
+norm = ImageNormalize(stretch=SqrtStretch())
+
+x_center = G_fit[1]
+y_center = G_fit[2]
+x_width = G_fit[3]
+y_width = G_fit[4]
+
 plt.figure()
 plt.imshow(Object1_Data, norm=norm, origin='lower', cmap='viridis')
-plt.show()
+plt.errorbar(x_center, y_center, xerr=x_width, yerr=y_width, ecolor='red')
 
+
+plt.show() # show all figures
+
+print('center: ',x_center, ',', y_center)
+print('width: ', 2*x_width, 'by', 2*y_width)
 
