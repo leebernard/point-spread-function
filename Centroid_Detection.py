a924bae66ce167beed2da1ab2cc4b66b67360be1
leebernard <leebernard@users.noreply.github.com>
2018-06-25 16:25:30 -0700

    updated to use new toolbox module

--- Centroid_Detection.py
+++ Centroid_Detection.py
@@ -1,84 +1,14 @@
 
 
-# This function takes a header data unit, find the bias data from BIASSEC, and performs bias calculations and
-# subtraction
-def bias_subtract(HDU):  # pass header data unit.  REMEBER, this is pass-by-reference
-    # import needed packages
-    # import numpy as np
-    # from astropy.io import fits
-    import re
-    from astropy.stats import sigma_clipped_stats
-
-    # Store the data from the HDU argument
-    im_data = HDU.data
-
-    # pull the bias section information
-    Bias_Sec = HDU.header['BIASSEC']
-    print('Bias Section is ' + Bias_Sec)
-    # print(type(Bias_Sec))
-    # slice the string, for converting to int
-    pattern = re.compile('\d+')  # pattern for all decimal digits
-    print(pattern.findall(Bias_Sec))
-
-    # hold the result in an object
-    match = pattern.findall(Bias_Sec)
-
-    # Bias section data from the header readout.
-    # image is not indexed the same as python.
-    # Image indexes (x,y), from lower left
-    # python indexes (y,x)
-
-    xmin = int(match[0])
-    xmax = int(match[1])
-    ymin = int(match[2])
-    ymax = int(match[3])
-
-    bias_data = im_data[ymin:ymax, xmin:xmax]
-
-    # Calculate the bias, using clipped statistics in case of cosmic ray events, and print the 		#results
-    bias_mean, bias_median, bias_std = sigma_clipped_stats(bias_data, sigma=3.0, iters=5)
-    print('Bias mean: ' + str(bias_mean))
-    print('Bias median: ' + str(bias_median))
-    print('Bias standerd deviation: ' + str(bias_std))
-
-    # calculate and print the bias area statistics, for reference.  DISABLED
-    # print('Bias area after subtraction \n Mean: ')
-    output_im = im_data - bias_mean
-    return output_im
-
-
-# calculates background using a mask routine from photutils. Requires passing a numpy array of image data
-def background_subtract(im_data):
-    # import numpy as np
-    # from astropy.io import fits
-
-    # store the data from the HDU argument
-    # im_data = HDU.data
-
-    # Generate mask
-    from photutils import make_source_mask
-    from astropy.stats import sigma_clipped_stats
-    mask = make_source_mask(im_data, snr=2, npixels=5, dilate_size=11)
-
-    # calculate bias using mean
-    # clipped stats are used, just in case
-    mean, median, std = sigma_clipped_stats(im_data, sigma=3.0, mask=mask)
-    print('Background mean: ' + str(mean))
-    print('Background median: ' + str(median))
-    print('Background standerd deviation: ' + str(std))
-
-    output_im = im_data - mean
-
-    return output_im, mask
-
-
 # needed packages
 import numpy as np
 # import matplotlib
 import matplotlib.pyplot as plt
 # import re
-
 from astropy.io import fits
+
+# import needed functions from the toolbox
+from ccd_tools import bias_subtract, background_subtract
 
 # open fits file, best practice
 file_name = '/home/lee/Documents/k4m_160319_101212_ori.fits.fz'
