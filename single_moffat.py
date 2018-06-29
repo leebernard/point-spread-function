fd92f7a92818d321049919f5269740f5d69c463f
leebernard <leebernard@users.noreply.github.com>
2018-06-28 17:10:18 -0700

    tried to fix sum of moffats by setting boundries upon the parameters, didnt work
    split off the single moffat version into it's own script, and it no long gives a reasonable answer

--- single_moffat.py
+++ single_moffat.py
@@ -1,5 +1,3 @@
-
-
 # needed packages
 import numpy as np
 # import matplotlib
@@ -98,17 +96,6 @@ def Moffat(indata, flux, x0, y0, alpha, beta, offset):
     return moffat_fun.ravel()
 
 
-def Moffat_sum(indata, flux1, flux2, alpha1, alpha2, beta1, beta2, x0, y0, offset):
-    x, y = indata
-    normalize1 = (beta1-1)/(np.pi*alpha1**2)
-    normalize2 = (beta2-1)/(np.pi*alpha2**2)
-
-    moffat1 = flux1*normalize1*(1 + ((x - x0)**2 + (y - y0)**2) / (alpha1**2))**(-beta1)
-    moffat2 = flux2*normalize2*(1 + ((x - x0)**2 + (y - y0)**2) / (alpha2**2))**(-beta2)
-    moffat_fun = offset + moffat1 + moffat2
-
-    return moffat_fun.ravel()
-
 # fit data to gaussian
 # instead fit data to moffat
 from scipy.optimize import curve_fit
@@ -119,38 +106,17 @@ x = np.arange(object1_data.shape[1])
 x, y = np.meshgrid(x, y)
 
 # generate a best guess
+flux_guess = np.amax(object1_data)
 y_guess = object1_data.shape[0]/2
 x_guess = object1_data.shape[1]/2
-amp1_guess = np.amax(object1_data)
-amp2_guess = np.mean(object1_data)
-beta1_guess = 1000
-beta2_guess = 2
-alpha1_guess = 2
-alpha2_guess = 20
+alpha_guess = 4
+beta_guess = 2
 offset_guess = 0
-guess = [amp1_guess, amp2_guess, alpha1_guess, alpha2_guess, beta1_guess, beta2_guess, x_guess, y_guess, offset_guess]
 
-# create bounds for the fit, in an attempt to keep it from blowing up
-"""
-flux1_bound = [0, np.inf]
-flux2_bound = [0, np.inf]
-alpha1_bound = [0.1, np.inf]
-alpha2_bound = [0.1, np.inf]
-beta1_bound = [1, np.inf]
-beta2_bound = [1, np.inf]
-x_bound = [0, object1_data.shape[1]]
-y_bound = [0, object1_data.shape[0]]
-offset_bound = [-np.inf, np.inf]
-"""
-# format the bounds
-lower_bounds = [0, 0, 0.1, 0.1, 1, 1, 0, 0, -np.inf]
-upper_bounds = [np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, object1_data.shape[1], object1_data.shape[0], np.inf]
+guess = [flux_guess, x_guess, y_guess, alpha_guess, beta_guess, offset_guess]
 
-bounds = (lower_bounds, upper_bounds)  # bounds set as pair of array-like tuples
-
-# curve fit
-# m_fit, m_cov = curve_fit(Moffat, (x,y), object1_data.ravel(), p0=[amp1_guess, x_guess, y_guess, 2, 2, 2])
-m_fit, m_cov = curve_fit(Moffat_sum, (x, y), object1_data.ravel(), p0=guess, bounds=bounds)
+# generate parameters for fit
+m_fit, m_cov = curve_fit(Moffat, (x, y), object1_data.ravel(), p0=guess)
 print('Resultant parameters')
 print(m_fit)
 
@@ -167,49 +133,36 @@ print(np.amax(object1_data))
 # display the result as a cross. The width of the lines correspond to the width in that direction
 norm = ImageNormalize(stretch=SqrtStretch())
 
-"""this is the center for the single moffat
+# this is the center for the single moffat
 x_center = m_fit[1]
 y_center = m_fit[2]
 x_width = m_fit[3] # alpha is the width
 y_width = m_fit[3]
-"""
-x_center = m_fit[6]
-y_center = m_fit[7]
-x_width = m_fit[3] # width of narrow moffet
-y_width = m_fit[2] # width of wide moffet
+
 plt.figure()
 plt.imshow(object1_data, norm=norm, origin='lower', cmap='viridis')
 plt.errorbar(x_center, y_center, xerr=x_width, yerr=y_width, ecolor='red')
-
-
-# print('center: ',x_center, ',', y_center)
-# print('width: ', 2*x_width, 'by', 2*y_width)
 
 
 """Chi squared calculations
 """
 observed = object1_data.ravel()
 
-# define the inputs for the Moffat
 m_input = (x, y)
-flux1 = m_fit[0]
-flux2 = m_fit[1]
-alpha1 = m_fit[2]
-alpha2 = m_fit[3]
-beta1 = m_fit[4]
-beta2 = m_fit[5]
-x0 = m_fit[6]
-y0 = m_fit[7]
-offset = m_fit[8]
-# expected = Moffat(m_input, flux, x0, y0, alpha, beta, offset) # old, for single Moffat
+flux = m_fit[0]
+x0 = m_fit[1]
+y0 = m_fit[2]
+alpha = m_fit[3]
+beta = m_fit[4]
+offset = m_fit[5]
 
-expected = Moffat_sum(m_input, flux1, alpha1, beta1, flux2, alpha2, beta2, x0, y0, offset)
+expected = Moffat(m_input, flux, x0, y0, alpha, beta, offset)
 
 # calculated raw chi squared
 chisq = sum(np.divide((observed - expected)**2, expected))
 
 # degrees of freedom, 5 parameters
-degrees_of_freedom = observed.size - 9
+degrees_of_freedom = observed.size - 6
 
 # normalized chi squared
 chisq_norm = chisq/degrees_of_freedom
