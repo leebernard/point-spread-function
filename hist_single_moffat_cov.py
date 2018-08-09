e05c70f5146cc39c835420e0940c5867a2089131
leebernard <leebernard@users.noreply.github.com>
2018-08-09 16:07:41 -0700

    Fixed the histogram titles

--- hist_single_moffat_cov.py
+++ hist_single_moffat_cov.py
@@ -31,17 +31,17 @@ def cov_to_coeff(cov):
     return coeff
 filename_list = []
 # filename = '/home/lee/Documents/single-moffat-archive-im7.pkl'
-# filename_list.append('/home/lee/Documents/single-moffat-archive-im1.pkl')
-# filename_list.append('/home/lee/Documents/single-moffat-archive-im2.pkl')
-# filename_list.append('/home/lee/Documents/single-moffat-archive-im4.pkl')
-# filename_list.append('/home/lee/Documents/single-moffat-archive-im5.pkl')
+filename_list.append('/home/lee/Documents/single-moffat-archive-im1.pkl')
+filename_list.append('/home/lee/Documents/single-moffat-archive-im2.pkl')
+filename_list.append('/home/lee/Documents/single-moffat-archive-im4.pkl')
+filename_list.append('/home/lee/Documents/single-moffat-archive-im5.pkl')
 filename_list.append('/home/lee/Documents/single-moffat-archive-im7.pkl')
-#
-# filename_list.append('/home/lee/Documents/single-moffat-archive-im9.pkl')
-# filename_list.append('/home/lee/Documents/single-moffat-archive-im10.pkl')
-# filename_list.append('/home/lee/Documents/single-moffat-archive-im12.pkl')
-# filename_list.append('/home/lee/Documents/single-moffat-archive-im13.pkl')
-# filename_list.append('/home/lee/Documents/single-moffat-archive-im16.pkl')
+
+filename_list.append('/home/lee/Documents/single-moffat-archive-im9.pkl')
+filename_list.append('/home/lee/Documents/single-moffat-archive-im10.pkl')
+filename_list.append('/home/lee/Documents/single-moffat-archive-im12.pkl')
+filename_list.append('/home/lee/Documents/single-moffat-archive-im13.pkl')
+filename_list.append('/home/lee/Documents/single-moffat-archive-im16.pkl')
 
 
 
@@ -66,8 +66,8 @@ for n, filename in enumerate(filename_list):
 
         # convert covariance matrix to correlation coeff
         coeff_mat = cov_to_coeff(cov_mat)
-        print('--------------------------')
-        print(np.array_str(coeff_mat, precision=2, suppress_small=True))
+        # print('--------------------------')
+        # print(np.array_str(coeff_mat, precision=2, suppress_small=True))
         # unpack the correlation coeff between the flux and other parameters
         fluxbeta_coeff.append(coeff_mat[0][3])
         fluxa_coeff.append(coeff_mat[0][4])
@@ -98,12 +98,17 @@ flux_beta_coeff = flux_hist.add_subplot(313)
 # np.linspace(-1, 0, num=251)
 
 # flux_a_coeff.hist(np.clip(fluxa_coeff, bins[0], bins[-1]), bins=bins, color='tab:blue')
-flux_a_coeff.hist(fluxa_coeff, bins=250, color='tab:blue')
-plt.title('Coeff of flux and width a')
-flux_b_coeff.hist(fluxb_coeff, bins=250, color='tab:blue')
-plt.title('Coeff of flux and width b')
-flux_beta_coeff.hist(fluxbeta_coeff, bins=250, color='tab:blue')
-plt.title('Coeff of flux and beta')
+flux_a_coeff.hist(fluxa_coeff, bins=np.linspace(-1, 0, num=251), color='tab:blue')
+flux_a_coeff.set_title('Coeff of flux and width a')
+flux_a_coeff.set_ylim(top=20)
+
+flux_b_coeff.hist(fluxb_coeff, bins=np.linspace(-1, 0, num=251), color='tab:blue')
+flux_b_coeff.set_title('Coeff of flux and width b')
+flux_b_coeff.set_ylim(top=20)
+
+flux_beta_coeff.hist(fluxbeta_coeff, bins=np.linspace(-1, 0, num=251), color='tab:blue')
+flux_beta_coeff.set_title('Coeff of flux and beta')
+flux_beta_coeff.set_ylim(top=20)
 
 
 width_hist = plt.figure('width coeff', figsize=(12, 10))
@@ -112,10 +117,16 @@ b_beta_coeff = width_hist.add_subplot(312)
 a_b_coeff = width_hist.add_subplot(313)
 
 # flux_a_coeff.hist(np.clip(fluxa_coeff, bins[0], bins[-1]), bins=bins, color='tab:blue')
-a_beta_coeff.hist(abeta_coeff, bins=250, color='tab:purple')
-plt.title('Coeff of width a and beta')
-b_beta_coeff.hist(bbeta_coeff, bins=250, color='tab:purple')
-plt.title('Coeff of width b and beta')
-a_b_coeff.hist(ab_coeff, bins=250, color='tab:purple')
-plt.title('Coeff of width a and width b')
+a_beta_coeff.hist(abeta_coeff, bins=np.linspace(0, 1, num=251), color='tab:purple')
+a_beta_coeff.set_title('Coeff of width a and beta')
+a_beta_coeff.set_ylim(top=20)
+
+b_beta_coeff.hist(bbeta_coeff, bins=np.linspace(0, 1, num=251), color='tab:purple')
+b_beta_coeff.set_title('Coeff of width b and beta')
+b_beta_coeff.set_ylim(top=20)
+
+a_b_coeff.hist(ab_coeff, bins=np.linspace(0, 1, num=251), color='tab:purple')
+a_b_coeff.set_title('Coeff of width a and width b')
+a_b_coeff.set_ylim(top=20)
+
 plt.show()
